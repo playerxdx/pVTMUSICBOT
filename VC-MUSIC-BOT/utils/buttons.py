f880cff0d2dc0b3cff011f66d3a0a4e4f218ca77
playerxdx <128796714+playerxdx@users.noreply.github.com>
2025-07-16 17:07:02 +0530

    Update buttons.py

--- VC-MUSIC-BOT/utils/buttons.py
+++ VC-MUSIC-BOT/utils/buttons.py
@@ -2,10 +2,20 @@
 
 from telethon import Button
 
-def get_control_buttons():
+def get_playback_buttons():
     return [
         [
             Button.inline("⏪ SeekBack", data="seekback"),
-            Button.inline("⏩ Seek", data="seek"),
+            Button.inline("⏸ Pause", data="pause"),
+            Button.inline("⏩ Seek", data="seek")
+        ],
+        [
+            Button.inline("🔁 Resume", data="resume"),
+            Button.inline("❌ Close", data="close")
         ]
+    ]
+
+def get_start_buttons():
+    return [
+        [Button.inline("📜 Help", data="show_help")]
     ]

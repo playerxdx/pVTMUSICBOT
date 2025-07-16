59adbf39c72c3b5f9a2909c91fbcf2c8f1a09817
playerxdx <128796714+playerxdx@users.noreply.github.com>
2025-07-16 16:59:24 +0530

    Create config.py

---
+++ VC-MUSIC-BOT/config.py
@@ -0,0 +1,8 @@
+# config.py
+
+API_ID = 12345678  # Replace with your own
+API_HASH = "abc123yourapihashhere"  # Replace with your own
+BOT_TOKEN = "your:bot_token_here"  # Replace with your own
+SESSION = "your_session_string_here"  # Replace with your own
+
+OWNER_ID = 123456789  # Your numeric Telegram ID

aac5f79bf75b361c7d7290bad7751576afce7217
playerxdx <128796714+playerxdx@users.noreply.github.com>
2025-07-16 19:21:05 +0530

    Update clients.py

--- VC-MUSIC-BOT/utils/clients.py
+++ VC-MUSIC-BOT/utils/clients.py
@@ -1,14 +1,10 @@
 import os
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
-from pytgcalls import PyTgCalls
+from pytgcalls import GroupCallFactory
 from config import API_ID, API_HASH, BOT_TOKEN, SESSION
 
-# Start the assistant (user) client using StringSession
 user = TelegramClient(StringSession(SESSION), API_ID, API_HASH).start()
-
-# Start the bot client using bot token
 bot = TelegramClient("bot", API_ID, API_HASH).start(bot_token=BOT_TOKEN)
+vc = GroupCallFactory(user).get_file_group_call()
 
-# Initialize PyTgCalls with the user (assistant) client
-vc = PyTgCalls(user)

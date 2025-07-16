2b155fc50ba22c8422bfb5924c2233049be0d3f9
playerxdx <128796714+playerxdx@users.noreply.github.com>
2025-07-16 18:07:23 +0530

    Update clients.py

--- VC-MUSIC-BOT/utils/clients.py
+++ VC-MUSIC-BOT/utils/clients.py
@@ -5,7 +5,7 @@ from config import API_ID, API_HASH, SESSION
 
 
 # Initialize assistant client (user account)
-user = TelegramClient(StringSession(STRING_SESSION), API_ID, API_HASH)
+user = TelegramClient(StringSession(SESSION), API_ID, API_HASH)
 user.start()
 
 # Initialize bot client (bot account)

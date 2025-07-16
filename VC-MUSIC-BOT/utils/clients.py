94159718ac4fd35339248f2780fc9b4f1197f2aa
playerxdx <128796714+playerxdx@users.noreply.github.com>
2025-07-16 18:03:40 +0530

    Update clients.py

--- VC-MUSIC-BOT/utils/clients.py
+++ VC-MUSIC-BOT/utils/clients.py
@@ -1,15 +1,15 @@
 import os
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
-from pytgcalls import PyTgCalls
-from config import API_ID, API_HASH, BOT_TOKEN, STRING_SESSION
+from config import API_ID, API_HASH, SESSION
+
 
 # Initialize assistant client (user account)
 user = TelegramClient(StringSession(STRING_SESSION), API_ID, API_HASH)
 user.start()
 
 # Initialize bot client (bot account)
-bot = TelegramClient("bot", API_ID, API_HASH).start(bot_token=BOT_TOKEN)
+bot = TelegramClient(StringSession(SESSION), API_ID, API_HASH).start()
 
 # Voice chat client
 vc = PyTgCalls(user)
